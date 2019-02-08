3e7b667888b9725cea74a2c1391fef6585a001d6
Mauricio Feldman-Abe <mauabe@gmx.com>
2019-02-07 19:58:18 -0800

    reat tutorial

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, {Component} from 'react';
+import Table from './Table';
+import Form from './Form';
+
+class App extends Component{
+  state = {
+    characters : []
+  };
+  // characters : [
+  //   {
+  //     'name': 'Charlie',
+  //     'job':'Janitor'
+  //   },
+  //   {
+  //     'name': 'Mac',
+  //     'job': 'Bouncer'
+  //   },
+  //   {        
+  //     'name': 'Dee',
+  //     'job': 'Aspiring Actress'
+  //   },
+  //   {        
+  //     'name': 'Dennis',
+  //     'job': 'Bartender'
+  //   }
+  // // ]
+
+  removeCharacter = index => {
+    const { characters } = this.state;
+
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index;
+      })
+    });
+  }
+
+  handleSubmit = character => {
+    this.setState({characters: [...this.state.characters, character]});
+  }
+  
+  
+  
+  render (){
+    const { characters } = this.state;
+    
+    return(
+      <div className = 'container'>
+        <h1>React Tutorial</h1>
+        <p>Add a character with a nme and job to the table</p>
+        <Table 
+          characterData={characters} 
+          removeCharacter={this.removeCharacter}
+        />
+        <h3>Add New</h3>
+        <Form handleSubmit={this.handleSubmit} />
+      </div>
+    );
+  }
+}
+                 
+export default App;
